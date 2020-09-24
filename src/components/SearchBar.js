00b49e1dda15603821993a7f038aeb871d983ec5
anahit1212 <alaverdyan.anahit@gmail.com>
2020-09-24 22:58:35 +0400

    Created Search Bar for searching pictures by their names

---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = {
+    term: "",
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <label>Image search</label>
+          <input
+            className="field"
+            placeholder="Type here to search images"
+            type="text"
+            value={this.state.term}
+            onChange={(e) => this.setState({ term: e.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
